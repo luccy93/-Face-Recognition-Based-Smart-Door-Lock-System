7fa1779dba0d29aca189ff2be882b657035ea1cb
Devendra prasad <devadraprasadkumar@gmail.com>
2026-02-23 18:26:22 +0530

    Add files via upload

---
+++ learing.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("C works in VS Code!");
+    return 0;
+}
---
+++ learning.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int myFavoriteNumer = 0;
+    printf("Please enter your favorite number\n ");
+    scanf("%d", &myFavoriteNumer);
+    printf("Your favorite number : %d\n", myFavoriteNumer);
+    return 0;
+}
